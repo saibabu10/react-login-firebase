8629e039316385269fe0e9dd7a14d3b43b3e6bb4
sai babu <saibabu@sais-Air-3.domain.name>
2024-02-23 21:13:23 +0530

    ADD.created two components Login and Register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import LoginFormComponent from './components/LoginFormComponent'
+import RegisterFormComponent from './components/RegisterFormComponent'
 function App() {
   return (
     <div className="App">
+      <LoginFormComponent/>
+      <RegisterFormComponent/>
     </div>
   );
 }
